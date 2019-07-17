6a80f6faa29ba6dd635baf8de79848986be21894
fxcm-pguz <pguz@fxcm.com>
2019-07-17 11:15:37 +0100

    final version of test

--- test.py
+++ test.py
@@ -20,40 +20,54 @@ def onMessage(event, message=''):
 	else:
 		logging.info("message: {0}:{1}".format(event,message))
 
-logging.basicConfig(
-	level=logging.INFO,
-	format="%(asctime)s %(message)s",
-	handlers=[
-		logging.FileHandler(args.logfile),
-		logging.StreamHandler(sys.stdout)
-	])
+def onDisconnect(code, reason=''):
+	logging.info("disconnected: {0} {1}".format(code, reason))
+	r.state = "disconnected: {0} {1}".format(code, reason)
 
-c = fxcmrest.Config(args.server, token=args.token)
-r = fxcmrest.FXCMRest(c)
-r.onMessage = onMessage
-r.connect()
-
-r.request('POST','/trading/subscribe', {'models':'Order'})
-r.account = r.request('GET','/trading/get_model',{'models':'Account'}).json()['accounts'][0]['accountId']
-
+count = 0
 while(True):
-	orderId = False
-	ok1 = False
-	ok2 = False
-	try:
-		orderId = r.request('POST','/trading/open_trade',{'account_id':r.account, 'symbol':'EUR/USD', 'is_buy':'true', 'amount':100, 'time_in_force':'FOK'}).json().get('data',{}).get('orderId',False)
-		time.sleep(3)
-		if r.positionId:
-			ok1 = r.request('POST','/trading/change_trade_stop_limit',{'trade_id':r.positionId, 'is_stop':'true', 'rate':-20}).json().get('response',{}).get('executed',False)
-			ok2 = r.request('POST','/trading/close_trade',{'trade_id':r.positionId, 'amount':100, 'order_type':'AtMarket', 'time_in_force':'FOK'}).json().get('response',{}).get('executed',False)
+	count += 1
+	logging.basicConfig(
+		level=logging.INFO,
+		format="%(asctime)s %(message)s",
+		handlers=[
+			logging.FileHandler("{0}-{1}.log".format(args.logfile,count)),
+			logging.StreamHandler(sys.stdout)
+		])
+	
+	while(True):
+		c = fxcmrest.Config(args.server, token=args.token)
+		r = fxcmrest.FXCMRest(c)
+		r.onMessage = onMessage
+		r.onDisconnect = onDisconnect
+		r.connect()
+		if not r.isConnected():
+			logging.info("could not connect. waiting...")
+			time.sleep(60*15)
 		else:
-			logging.info("positionId not found!")
 			break
-		if ok1 and ok2:
-			time.sleep(60*10)
-		else:
-			logging.info("trade change or trade close failed")
+	
+	r.request('POST','/trading/subscribe', {'models':'Order'})
+	r.account = r.request('GET','/trading/get_model',{'models':'Account'}).json()['accounts'][0]['accountId']
+	
+	while(True):
+		if not r.isConnected():
+			logging.info("socket.io disconnected. disconnecting")
 			break
-	except Exception as e:
-		logging.info("exception: {0}".format(e))
-		break
+		orderId = False
+		ok1 = False
+		ok2 = False
+		try:
+			orderId = r.request('POST','/trading/open_trade',{'account_id':r.account, 'symbol':'EUR/USD', 'is_buy':'true', 'amount':100, 'time_in_force':'FOK'}).json().get('data',{}).get('orderId',False)
+			time.sleep(3)
+			if r.positionId:
+				ok1 = r.request('POST','/trading/change_trade_stop_limit',{'trade_id':r.positionId, 'is_stop':'true', 'rate':-20}).json().get('response',{}).get('executed',False)
+				ok2 = r.request('POST','/trading/close_trade',{'trade_id':r.positionId, 'amount':100, 'order_type':'AtMarket', 'time_in_force':'FOK'}).json().get('response',{}).get('executed',False)
+				if not (ok1 and ok2):
+					logging.info("trade change or trade close failed")
+				time.sleep(60*10)
+			else:
+				logging.info("positionId not found!")
+		except Exception as e:
+			logging.info("exception: {0}".format(e))
+			break
